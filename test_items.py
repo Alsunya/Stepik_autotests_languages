2ce9caeb4aa473a82d4f837114410a6124beee47
Alsu Shaykhidinova <Shaykhidinova@avp.ru>
2021-09-05 23:18:47 +0300

    Поменялась структура assert

--- test_items.py
+++ test_items.py
@@ -2,9 +2,6 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 #link = "https://selenium1py.pythonanywhere.com"
 def test_is_there_a_button(browser):
-    try:
-        browser.get(link)
-        browser.find_element_by_css_selector(".btn-add-to-basket")
-        assert True
-    finally:
-        time.sleep(8)
+    browser.get(link)
+    assert browser.find_element_by_css_selector('.btn-add-to-basket')
+    time.sleep(8)
